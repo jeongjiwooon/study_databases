1982a03c37d890ef165f28557470eb529f13d385
jeongjiwooon <jjwanm@naver.com>
2023-06-07 17:40:04 +0900

    commit

--- sqls/whereOrderGroupby.sql
+++ sqls/whereOrderGroupby.sql
@@ -1,8 +1,9 @@
-SELECT count(*)
-FROM orders
-group by EmployeeID in (7, 9);
+select EmployeeID, count(*) as count_orders
+from orders
+where EmployeeID in (7,9)
+group by EmployeeID;
 
 -- Number of Records: 2
--- count(*)
--- 176
--- 20
+-- EmployeeID	count_orders
+-- 7	14
+-- 9	6
