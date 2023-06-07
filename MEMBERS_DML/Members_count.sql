2bbac25f4c6dc52a140fcf0316f8848f74c76cf9
jeongjiwooon <jjwanm@naver.com>
2023-06-07 18:06:05 +0900

    commit

--- MEMBERS_DML/Members_count.sql
+++ MEMBERS_DML/Members_count.sql
@@ -13,3 +13,13 @@
 select count(*), GENDER_CODE_ID, min(NAME)
 from members
 group by GENDER_CODE_ID;
+
+-- select EmployeeID, count(*) as count_orders
+-- from orders
+-- where EmployeeID in (7,9)
+-- group by EmployeeID;
+
+-- select count(*) as employee_cnt, t_ors.employeeid
+-- from orders as t_ors
+-- where t_ors.EmployeeID in (7,9)
+-- group by t_ors.EmployeeID;
