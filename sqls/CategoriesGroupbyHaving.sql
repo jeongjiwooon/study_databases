4f7456eea93db4f4ededba55c779665f6eee8d51
jeongjiwooon <jjwanm@naver.com>
2023-06-08 11:05:17 +0900

    commit

--- sqls/CategoriesGroupbyHaving.sql
+++ sqls/CategoriesGroupbyHaving.sql
@@ -2,3 +2,11 @@ select count(category.categoryid) as cnt_category, category.categoryid
 from Products as category
 group by category.categoryid
 having count(category.categoryid) >= 10;
+-- Number of Records: 5
+-- Number of Records: 5
+-- cnt_category	CategoryID
+-- 12	1
+-- 12	2
+-- 13	3
+-- 10	4
+-- 12	8
