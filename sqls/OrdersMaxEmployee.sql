894ea72b7e3464998205d7176f2e4577f529d577
jeongjiwooon <jjwanm@naver.com>
2023-06-08 17:31:54 +0900

    commit

---
+++ sqls/OrdersMaxEmployee.sql
@@ -0,0 +1,10 @@
+select max(cnt.total_ord) as max_total_ord, cnt.LastName, cnt.FirstName
+from
+(
+    select count(ord.OrderID) as total_ord, emp.LastName, emp.FirstName
+    from Employees as emp
+    inner join Orders as ord
+    on emp.EmployeeID = ord.EmployeeID
+    group by emp.EmployeeID
+)
+as cnt;
